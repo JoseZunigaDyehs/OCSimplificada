89193f6cac0d11b2cb5273e74ffa652cad42f58e
Jose Zuñiga <zuniga.joseignacio@gmail.com>
2020-06-02 22:00:14 -0400

    without warnings

--- src/screens/OC/Steps/Step2.js
+++ src/screens/OC/Steps/Step2.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
-import { useRegionComunas, useOrden } from 'context'
 import { TextInput, RadioButton } from 'components/fieldsForm'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(({ spacing, palette, breakpoints }) => ({
+const useStyles = makeStyles(({ spacing, breakpoints }) => ({
 	title: {
 		paddingBottom: spacing(2),
 	},
@@ -28,55 +27,9 @@ function Step2({
 	fieldsById,
 	onChange,
 	onFocusHandle,
-	setModalConfig,
+	//setModalConfig,
 }) {
 	const classes = useStyles()
-	const [comunas, setComunas] = useState([])
-	const [comunaLabel, setComunaLabel] = useState('')
-	const {
-		orden: {
-			regionId,
-			regionLabel,
-			direccionDespacho,
-			direccionesDespacho,
-			withDireccionDespacho,
-			diasHabiles,
-		},
-	} = useOrden()
-	const { getComunas } = useRegionComunas()
-	const openModal = () => {
-		setModalConfig({
-			show: true,
-			type: 'direction',
-			data: {
-				regionLabel,
-				direcciones: direccionesDespacho,
-				comunas,
-				withUpsert: false,
-			},
-		})
-	}
-	useEffect(() => {
-		const getcomunasAPI = async () => {
-			try {
-				const nextComunas = await getComunas(regionId)
-				setComunas(nextComunas)
-			} catch (e) {
-				console.log(e)
-				//ERROR
-			}
-		}
-		getcomunasAPI()
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
-	useEffect(() => {
-		if (comunas.length > 0) {
-			const nextComunaLabel = direccionDespacho
-				? comunas.find(x => x.id === direccionDespacho.comunaId).label
-				: ''
-			setComunaLabel(nextComunaLabel)
-		}
-	}, [direccionDespacho, comunas])
 	return (
 		<Grid container direction="column">
 			<Typography variant="h5" className={classes.title}>
@@ -94,7 +47,7 @@ function Step2({
 				</Grid>
 				<Grid item md={4} xs={12}>
 					<TextInput
-						{...fieldsById.despacho_observacion}
+						{...fieldsById.pago_observacion}
 						onChange={onChange}
 						onFocusHandle={onFocusHandle}
 					/>
